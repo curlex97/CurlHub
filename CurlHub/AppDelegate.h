92296f2d7272ef35fa1cd0dd2138fb0264f41b3d
Arthur Chystyak <artur.chistyak@outlook.com>
2016-07-24 23:58:34 +0300

    Initial Commit

---
+++ CurlHub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CurlHub
+//
+//  Created by Arthur Chistyak on 24.07.16.
+//  Copyright © 2016 ArthurChistyak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CurlHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CurlHub
+//
+//  Created by Arthur Chistyak on 24.07.16.
+//  Copyright © 2016 ArthurChistyak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
